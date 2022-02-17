fa1640b359abc54ef69ebdf776d34d6f1230a1f7
liuxiang7 <liuxiang7@tal.com>
2022-02-17 22:49:57 +0800

    feat: 获取商品数据并table显示

--- src/views/page/productList.vue
+++ src/views/page/productList.vue
@@ -2,11 +2,15 @@
   <div class="product-list">
         <!-- 搜索 -->
         <search-box @submit="searchSubmit" :data="categoryList"/>
+        <!-- 表格 -->
+        <ProductTable :data="tableData" />
     </div>
 </template>
 <script>
 import searchBox from '@/components/search.vue';
+import ProductTable from '@/components/productTable.vue';
 import categoryApi from '@/api/category';
+import api from '@/api/product';
 
 export default {
   data() {
@@ -19,19 +23,40 @@ export default {
   },
   components: {
     searchBox,
+    ProductTable,
   },
   async created() {
     await categoryApi.list().then((res) => {
       this.categoryList = res.data;
-      res.data.forEach((item) => {
-        this.categoryObj[item.id] = item;
-      });
+      console.log(
+        "categoryApi", this.categoryList
+      )
+      // res.data.forEach((item) => {
+      //   this.categoryObj[item.id] = item;
+      // });
     });
+    this.getTableData()
   },
   methods: {
     searchSubmit(form) {
-      console.log("searchSubmit", form)
+      console.log('searchSubmit', form);
       this.searchForm = form;
+    },
+    getTableData() {
+      api
+        .list({
+          // page: this.page.current,
+          // size: this.page.pageSize,
+          ...this.searchForm,
+        })
+        .then((res) => {
+          console.log(res);
+          // this.page.total = res.total;
+          this.tableData = res.data.map((item) => ({
+            ...item,
+            // categoryName: this.categoryObj[item.category].name,
+          }));
+        });
     },
   },
 };
