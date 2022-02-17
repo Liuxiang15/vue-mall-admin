774b835f6c0a72fa16a646475af522d4759c35c1
liuxiang7 <liuxiang7@tal.com>
2022-02-17 22:08:40 +0800

    feat:搜索框和获取商品类名

--- axios.js
+++ axios.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+// import store from 'store/index';
+import store from '@/store/index';
 
 const instance = axios.create({
   baseURL: 'https://mallapi.duyiedu.com/',
@@ -6,7 +8,24 @@ const instance = axios.create({
 
 // 请求拦截
 instance.interceptors.request.use(
-  (config) => config,
+  (config) => {
+    // 这些请求是不需要加appkey的
+    if (config.url.includes('/passport')) {
+      return config;
+    }
+    // 这种写法会报错TypeError: Cannot set properties of undefined (setting 'appkey')
+    // const copy = config;
+    // copy.params.appkey = store.state.user.appkey;
+    // return config;
+
+    return {
+      ...config,
+      params: {
+        ...config.params,
+        appkey: store.state.user.appkey,
+      },
+    };
+  },
   (error) => Promise.reject(error),
 );
 
--- src/views/page/productList.vue
+++ src/views/page/productList.vue
@@ -1,3 +1,38 @@
 <template>
-  <div>商品列表</div>
+  <div class="product-list">
+        <!-- 搜索 -->
+        <search-box @submit="searchSubmit" :data="categoryList"/>
+    </div>
 </template>
+<script>
+import searchBox from '@/components/search.vue';
+import categoryApi from '@/api/category';
+
+export default {
+  data() {
+    return {
+      tableData: [],
+      searchForm: {},
+      categoryList: [],
+      categoryObj: {},
+    };
+  },
+  components: {
+    searchBox,
+  },
+  async created() {
+    await categoryApi.list().then((res) => {
+      this.categoryList = res.data;
+      res.data.forEach((item) => {
+        this.categoryObj[item.id] = item;
+      });
+    });
+  },
+  methods: {
+    searchSubmit(form) {
+      console.log("searchSubmit", form)
+      this.searchForm = form;
+    },
+  },
+};
+</script>
