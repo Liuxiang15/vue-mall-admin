eb31c72ace33487d54c05795c1940f6eddf0a688
liuxiang7 <liuxiang7@tal.com>
2022-02-19 18:06:22 +0800

    feat:添加商品

--- src/api/product.js
+++ src/api/product.js
@@ -7,4 +7,14 @@ export default {
   remove(params) {
     return axios.delete(`/products/${params.id}`);
   },
+  add(params) {
+    return axios.post('/products/add', params);
+  },
+  detail(id) {
+    return axios.get(`/products/${id}`);
+  },
+  edit(params) {
+    console.log(params);
+    return axios.put('/products/edit', params);
+  },
 };
